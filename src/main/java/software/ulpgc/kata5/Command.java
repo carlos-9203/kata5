94173264c51c4ba3316f8432a919ea7d02afd275
Carlos <carlosalonso9797@gmail.com>
2024-01-21 13:02:25 +0000

    esta hecha la interfaz Command

---
+++ src/main/java/software/ulpgc/kata5/Command.java
@@ -0,0 +1,16 @@
+package software.ulpgc.kata5;
+
+
+
+public interface Command {
+    Output execute(Input input);
+
+    interface Input{
+        String get(String parameter);
+    }
+
+    interface Output {
+        int responseCode();
+        String result();
+    }
+}
